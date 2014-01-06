09eaaf4dcb56d65cd5ef76ebf7b840676b4e6b21
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2014-01-06 14:40:29 +0100

    better model

--- src/main/scala/code/model/User.scala
+++ src/main/scala/code/model/User.scala
@@ -3,12 +3,13 @@ package code.model
 import net.liftweb.mongodb._
 import net.liftweb.mongodb.record._
 import net.liftweb.mongodb.record.field._
+import net.liftweb.record.field.{ IntField , StringField }
 
 class User private() extends MongoRecord[User] with ObjectIdPk[User] {
   def meta = User
 
-  object name //extends StringField(this, 12)
-  object yob //extends IntField(this)
+  object name extends StringField(this, 12)
+  object yob extends IntField(this)
 }
 
 object User extends User with MongoMetaRecord[User]
